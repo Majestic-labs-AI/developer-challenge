03dd4dc56938ec12621582d70daaf3e6b18529a8
Brian Cunnie <brian.cunnie@gmail.com>
2024-11-25 05:37:14 -0800

    `main.py` has constructor, skeleton main
    Although this developer challenge is slightly easier in that it's more
    opinionated (e.g. `traverse()` is a class method not a function, root node
    is `a`), I don't think it detracts from the validity of the challenge.

---
+++ main.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def traverse(self):
+        # WRITE TRAVERSAL CODE HERE
+        pass
+        
+
+def main():
+    a = Node('a')
+    # INITIALIZE THE REMAINDER OF THE TREE HERE
+
+if __name__ == "__main__":
+    main()
